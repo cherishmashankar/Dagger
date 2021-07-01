74ad3d12ab6429985fc1a01a272b59ee26f26036
unknown <cherry.sh1996@gmail.com>
2021-07-01 08:52:47 +0530

    Custom Scope

--- app/src/main/java/com/example/android/dagger/di/ActivityBuildersModule.java
+++ app/src/main/java/com/example/android/dagger/di/ActivityBuildersModule.java
@@ -2,9 +2,11 @@ package com.example.android.dagger.di;
 
 
 import com.example.android.dagger.di.auth.AuthModule;
+import com.example.android.dagger.di.auth.AuthScope;
 import com.example.android.dagger.di.auth.AuthViewModelsModule;
 import com.example.android.dagger.di.main.MainFragmentBuildersModule;
 import com.example.android.dagger.di.main.MainModule;
+import com.example.android.dagger.di.main.MainScope;
 import com.example.android.dagger.di.main.MainViewModelModule;
 import com.example.android.dagger.network.main.MainApi;
 import com.example.android.dagger.ui.auth.AuthActivity;
@@ -16,10 +18,12 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule  {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelsModule.class , AuthModule.class})
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuildersModule.class, MainViewModelModule.class, MainModule.class}
     )
